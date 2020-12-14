fb784e43960311630f00230e4afd668e1495982d
Nicolas Neudeck <nicolas.neudeck@eon.com>
2020-12-14 23:07:57 +0100

    add api key and stuff

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "7a0252cb8ca0a067e4155e9803bc9f62";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
